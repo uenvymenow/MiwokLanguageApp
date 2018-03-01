5c71ed88785034e322d1178fc9e4ab4b9c1fe561
unknown <uenvymenow@gmail.com>
2018-03-01 16:03:30 -0500

    Added functionality to access NumbersActivity from MainActivity

--- Miwok Starter Code/app/src/main/java/com/example/android/miwok/ColorActivity.java
+++ Miwok Starter Code/app/src/main/java/com/example/android/miwok/ColorActivity.java
@@ -8,6 +8,6 @@ public class ColorActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_color);
+        setContentView(R.layout.activity_colors);
     }
 }
--- Miwok Starter Code/app/src/main/java/com/example/android/miwok/PhrasesActivity.java
+++ Miwok Starter Code/app/src/main/java/com/example/android/miwok/PhrasesActivity.java
@@ -8,6 +8,6 @@ public class PhrasesActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_phrases2);
+        setContentView(R.layout.activity_phrases);
     }
 }
