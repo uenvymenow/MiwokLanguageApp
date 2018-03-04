8134fc754fc0905a26c704b9590c8d7eb3c164b2
uenvymenow <uenvymenow@gmail.com>
2018-03-04 14:45:18 -0600

    Added TextView for each item in ArrayList using the for loop

--- Miwok Starter Code/app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ Miwok Starter Code/app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -4,6 +4,9 @@ import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
@@ -27,19 +30,16 @@ public class NumbersActivity extends AppCompatActivity {
         numbersListEnglish.add("Nine");
         numbersListEnglish.add("Ten");
 
+        // Find rootView LinearLayout from activity_numbers.xml
+        LinearLayout numbersView = findViewById(R.id.rootView);
 
-        // Verify content of array
-        Log.i("NumbersActivity", "Word at index 0: " + numbersListEnglish.get(0));
-        Log.i("NumbersActivity", "Word at index 1: " + numbersListEnglish.get(1));
-        Log.i("NumbersActivity", "Word at index 2: " + numbersListEnglish.get(2));
-        Log.i("NumbersActivity", "Word at index 3: " + numbersListEnglish.get(3));
-        Log.i("NumbersActivity", "Word at index 4: " + numbersListEnglish.get(4));
-        Log.i("NumbersActivity", "Word at index 5: " + numbersListEnglish.get(5));
-        Log.i("NumbersActivity", "Word at index 6: " + numbersListEnglish.get(6));
-        Log.i("NumbersActivity", "Word at index 7: " + numbersListEnglish.get(7));
-        Log.i("NumbersActivity", "Word at index 8: " + numbersListEnglish.get(8));
-        Log.i("NumbersActivity", "Word at index 9: " + numbersListEnglish.get(9));
-
+        // For loop that runs for each element in the numbersListEnglish ArrayList
+        for (int index = 0; index < numbersListEnglish.size(); index++) {
+            // Setup instructions to run each time through
+            TextView wordView = new TextView(this);
+            wordView.setText(numbersListEnglish.get(index));
+            numbersView.addView(wordView);
+        }
     }
 
 }
