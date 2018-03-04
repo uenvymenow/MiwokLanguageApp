bc4a9b4f4005f39100550a679a8934456958afef
uenvymenow <uenvymenow@gmail.com>
2018-03-04 00:58:03 -0600

    Modified the Array in the NumbersActivity to be an ArrayList

--- Miwok Starter Code/app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ Miwok Starter Code/app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -4,8 +4,7 @@ import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Toast;
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -14,20 +13,32 @@ public class NumbersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
 
-        // Create an array for the number list
-        String[] numbers = new String[]{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
+        // Create an ArrayList for the numbers
+        ArrayList<String> numbersListEnglish = new ArrayList<String>();
+
+        numbersListEnglish.add("One");
+        numbersListEnglish.add("Two");
+        numbersListEnglish.add("Three");
+        numbersListEnglish.add("Four");
+        numbersListEnglish.add("Five");
+        numbersListEnglish.add("Six");
+        numbersListEnglish.add("Seven");
+        numbersListEnglish.add("Eight");
+        numbersListEnglish.add("Nine");
+        numbersListEnglish.add("Ten");
+
 
         // Verify content of array
-        Log.i("NumbersActivity", "Word at index 0: " + numbers[0]);
-        Log.i("NumbersActivity", "Word at index 1: " + numbers[1]);
-        Log.i("NumbersActivity", "Word at index 2: " + numbers[2]);
-        Log.i("NumbersActivity", "Word at index 3: " + numbers[3]);
-        Log.i("NumbersActivity", "Word at index 4: " + numbers[4]);
-        Log.i("NumbersActivity", "Word at index 5: " + numbers[5]);
-        Log.i("NumbersActivity", "Word at index 6: " + numbers[6]);
-        Log.i("NumbersActivity", "Word at index 7: " + numbers[7]);
-        Log.i("NumbersActivity", "Word at index 8: " + numbers[8]);
-        Log.i("NumbersActivity", "Word at index 9: " + numbers[9]);
+        Log.i("NumbersActivity", "Word at index 0: " + numbersListEnglish.get(0));
+        Log.i("NumbersActivity", "Word at index 1: " + numbersListEnglish.get(1));
+        Log.i("NumbersActivity", "Word at index 2: " + numbersListEnglish.get(2));
+        Log.i("NumbersActivity", "Word at index 3: " + numbersListEnglish.get(3));
+        Log.i("NumbersActivity", "Word at index 4: " + numbersListEnglish.get(4));
+        Log.i("NumbersActivity", "Word at index 5: " + numbersListEnglish.get(5));
+        Log.i("NumbersActivity", "Word at index 6: " + numbersListEnglish.get(6));
+        Log.i("NumbersActivity", "Word at index 7: " + numbersListEnglish.get(7));
+        Log.i("NumbersActivity", "Word at index 8: " + numbersListEnglish.get(8));
+        Log.i("NumbersActivity", "Word at index 9: " + numbersListEnglish.get(9));
 
     }
 
